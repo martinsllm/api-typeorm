884a585d581ce050e6dae038e74f6a4ea7302b52
martinsllm <lucaslm2010@gmail.com>
2024-05-27 21:36:31 -0300

    Initial commit

---
+++ src/data-source.ts
@@ -0,0 +1,19 @@
+import 'dotenv/config';
+import { DataSource } from 'typeorm';
+
+const AppDataSource = new DataSource({
+    type: "mysql",
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT) || 3306,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+})
+
+AppDataSource.initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization", err)
+    });
---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import './data-source';
+import app from './app';
+import 'dotenv';
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running in port ${PORT}!`)
+})
