b2ed947205f46d3e013bcdebe01351ac3499ad62
martinsllm <lucaslm2010@gmail.com>
2024-06-01 11:17:19 -0300

    Creating user's table

---
+++ src/database/migrations/1717251402191-default.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Default1717251402191 implements MigrationInterface {
+    name = 'Default1717251402191'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE \`user\` (\`id\` int NOT NULL AUTO_INCREMENT, \`email\` varchar(255) NOT NULL, \`password\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE \`user\``);
+    }
+
+}
